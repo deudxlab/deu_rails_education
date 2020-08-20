ff8d8210864772ec2e37830e215c426d3ec00d15
kimchijoa <amsmdmfm159@naver.com>
2020-08-20 06:41:57 +0000

    first

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :class_statuses
+  resources :class_lists
+  devise_for :users
+  resources :comments
+  resources :articles
+  resources :tests
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	
+  root 'class_lists#index'
+end
